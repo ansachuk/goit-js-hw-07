1ca9c32b3a1f3d72bfe6a929be0abb8f567df557
ansachuk <ansachuk@icloud.com>
2023-03-03 19:56:37 +0200

    not finished task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const sipleGalleryListRef = document.querySelector(".gallery");
+
+const sipleGalleryMarkup = galleryItems
+  .map(
+    ({
+      original,
+      preview,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+
+sipleGalleryListRef.insertAdjacentHTML("beforeend", sipleGalleryMarkup);
+
+sipleGalleryListRef.addEventListener("click", handlerGalleryItemClick);
+
+function handlerGalleryItemClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    fadeSpeed: 400,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  lightbox.open();
+
+  console.log(e);
+
+  function handlerInstanceClose(e) {
+    e.preventDefault();
+  }
+
+  window.addEventListener("keydown", handlerInstanceClose);
+}
