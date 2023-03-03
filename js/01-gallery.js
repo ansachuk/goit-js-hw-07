bfd1f3c8bf4cf76a95b5681723be3d741b621a08
ansachuk <ansachuk@icloud.com>
2023-03-03 17:29:39 +0200

    finish first part of hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const basicGalleryListRef = document.querySelector(".gallery");
+
+const basicGalleryMarkup = galleryItems
+  .map(
+    ({ original, preview, description }) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        target="_blank" rel="noopener noreferrer"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
+
+basicGalleryListRef.insertAdjacentHTML("beforeend", basicGalleryMarkup);
+
+basicGalleryListRef.addEventListener("click", handlerGalleryItemClick);
+
+function handlerGalleryItemClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  function handlerInstanceClose(e) {
+    e.preventDefault();
+    if (e.code === "Escape") {
+      instance.close(
+        window.removeEventListener("keydown", handlerInstanceClose)
+      );
+    }
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`
+  );
+
+  instance.show();
+
+  window.addEventListener("keydown", handlerInstanceClose);
+}
